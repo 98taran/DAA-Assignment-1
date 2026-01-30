8861ffa9419c2009603d4fa51161bd303c861bb0
Tarandeep singh <tarandeep4994@gmail.com>
2026-01-30 10:28:34 +0530

    q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -1,44 +1,45 @@
-//quick sort
-
 #include <iostream>
 using namespace std;
 
-int partition(int A[],int l,int h){
-    int pivot=A[l];
-    int i=l,j=h;
-    while(i<j){
-        do{
-            i++;
-        }while(i<h && A[i]<=pivot);
+int partition(int arr[], int start, int end) {
+    int pivot = arr[start];
+    int lft = start;
+    int rt = end;
 
-        do{
-            j--;
-        }while(A[j]>pivot);
+    while (lft < rt) {
+        do {
+            lft++;
+        } while (lft < end && arr[lft] <= pivot);
 
-        if(i<j){
-            swap(A[i],A[j]);
+        do {
+            rt--;
+        } while (arr[rt] > pivot);
+
+        if (lft < rt) {
+            swap(arr[lft], arr[rt]);
         }
     }
-    swap(A[l],A[j]);
-    return j;
 
+    swap(arr[start], arr[rt]);
+    return rt;
 }
 
-void quickSort(int A[],int l,int h){
-    if(l<h){
-        int j=partition(A,l,h);
-        quickSort(A,l,j);
-        quickSort(A,j+1,h);
+void quickSort(int arr[], int start, int end) {
+    if (start < end) {
+        int pos = partition(arr, start, end);
+        quickSort(arr, start, pos);
+        quickSort(arr, pos + 1, end);
     }
 }
 
-int main(){
-    int arr[]= {2,9,7,5,12,10};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr,0,n);
+int main() {
+    int arr[6] = {2, 9, 7, 5, 12, 10};
+    int size = 6;
 
-    for(int i=0;i<n;i++)
-        cout<<arr[i]<<" ";
+    quickSort(arr, 0, size);
+
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
